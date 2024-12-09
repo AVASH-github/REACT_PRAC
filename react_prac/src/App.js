ee1f565a673fb2918b93d1d9d85d428567d59a20
AVASH-github <148526107+AVASH-github@users.noreply.github.com>
2024-12-09 08:13:44 +0000

    making of tic tac toe game

--- react_prac/src/App.js
+++ react_prac/src/App.js
@@ -1,17 +1,9 @@
-import ButtonClick from "./buttonClick";
-import { useState } from "react";
+import Board from "./tic-tac-toe";
 function App() {
-  const [count,setCount]=useState(0);
-
-  function HandleClick(){
-      setCount(count+1);
-  }
-
   return (
     <>
-    <h1>Counters that works together </h1>
-    <ButtonClick count={count} onClick={HandleClick} />
-    <ButtonClick count={count} onClick={HandleClick} />
+    
+   <Board />
     
     </>
   );
---
+++ react_prac/src/tic-tac-toe.jsx
@@ -0,0 +1,32 @@
+    import {useState} from 'react';
+function Square({value,onSquareClick}){
+    return( 
+    <button className="square" onClick={onSquareClick}>
+        {value}
+    </button>
+    );
+}
+export default function Board(){
+    const [square,setSquare]=useState(Array(9).fill(null));
+
+    return (
+        <>
+        <div className="board-row"> 
+        <Square value={square[0]}/>
+        <Square value={square[1]}/>
+        <Square value={square[2]}/>
+        </div>
+        <div className="board-row"> 
+        <Square value={square[3]}/>
+        <Square value={square[4]}/>
+        <Square value={square[5]}/>
+        </div>
+        <div className="board-row"> 
+        <Square value={square[6]}/>
+        <Square value={square[7]}/>
+        <Square value={square[8]}/>
+        </div>
+        </>
+            );
+}
+
