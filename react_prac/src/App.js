d9c69a6beba8341478d2310ece19e38965a2e69a
AVASH-github <148526107+AVASH-github@users.noreply.github.com>
2024-12-10 07:05:33 +0000

    added grocery

--- react_prac/src/App.js
+++ react_prac/src/App.js
@@ -1,10 +1,21 @@
 import Game from "./tic-tac-toe";
+import GroceryForm from "./GroceryForm";
+import GroceryList from "./GroceryList";
+import { useState } from "react";
 function App() {
+  const [groceries,setGroceries]=useState([]);
+
+
+  const addGroceries = (item) => {
+    setGroceries([...groceries, item]); 
+  };
+
   return (
     <>
     
  <Game />
-    
+    <GroceryForm addGroceries={addGroceries}/>
+    <GroceryList groceries={groceries}/>
     </>
   );
 }
